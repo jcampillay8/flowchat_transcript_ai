18b70edba7740cd5bfb64d9a3d8bee8d81d031c6
jcampillay <jcampillayworks@gmail.com>
2026-03-28 17:36:39 -0300

    ajuste media_processor

--- src/services/media_processor.py
+++ src/services/media_processor.py
@@ -65,10 +65,17 @@ class MediaProcessorService:
                         'preferredcodec': 'wav',
                         'preferredquality': '192',
                     }],
-                    # 'outtmpl' define el nombre base. yt-dlp añade '.wav' al procesar.
                     'outtmpl': temp_audio_final.replace('.wav', ''),
                     'quiet': True,
                     'no_warnings': True,
+                    # --- AJUSTE PARA EVITAR BLOQUEO DE BOT ---
+                    'extractor_args': {
+                        'youtube': {
+                            'player_client': ['ios', 'android'],
+                            'skip': ['dash', 'hls']
+                        }
+                    },
+                    'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
                 }
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     ydl.download([source_value])
